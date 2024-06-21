89c990fa9a08cf5451a60644bd14cc86bfaa1a29
Frederick Roy <froy@lnrobo.com>
2024-06-22 07:58:26 +0900

    WIP

--- SofaGLFW/src/SofaGLFW/SofaGLFWBaseGUI.h
+++ SofaGLFW/src/SofaGLFW/SofaGLFWBaseGUI.h
@@ -32,6 +32,8 @@
 
 #include <memory>
 
+#include <bgfx/bgfx.h>
+
 struct GLFWwindow;
 struct GLFWmonitor;
 
@@ -94,6 +96,12 @@ public:
     {
         return m_guiEngine;
     }
+    
+    bool initEngine(uint32_t width, uint32_t height, GLFWwindow* glfwWindow);
+    bgfx::RendererType::Enum m_type = bgfx::RendererType::OpenGL; // obviously to change
+    uint16_t m_pciId = BGFX_PCI_ID_APPLE;
+    uint32_t m_debug = BGFX_DEBUG_NONE;
+    uint32_t m_reset = BGFX_RESET_VSYNC;
     
 private:
     // GLFW callbacks
